0a8d321bdc2f40d3fa6ae6fe6585ac3bfc1d1e87
Aditya Kumar Bodapati <aditya@tilicho.in>
2023-02-23 16:57:20 +0530

    Project setup

---
+++ FlexChatBox/Sources/FlexChatBox/FlexChatBox.swift
@@ -0,0 +1,6 @@
+public struct FlexChatBox {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ FlexChatBox/Tests/FlexChatBoxTests/FlexChatBoxTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FlexChatBox
+
+final class FlexChatBoxTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FlexChatBox().text, "Hello, World!")
+    }
+}
